b74661c8c5c401ca5a4d3428cfa3af73f461b2e3
sanderegg <35365065+sanderegg@users.noreply.github.com>
2022-09-16 10:24:37 +0200

    fix test

--- tests/integration/test_docker_container.py
+++ tests/integration/test_docker_container.py
@@ -8,11 +8,12 @@ import os
 import shutil
 from pathlib import Path
 from pprint import pformat
-from typing import Dict
-
-import pytest
+from typing import Dict, Iterator
 
 import docker
+import docker.errors
+import docker.models.containers
+import pytest
 
 _FOLDER_NAMES = ["input", "output"]
 _CONTAINER_FOLDER = Path("/home/scu/data")
@@ -58,7 +59,7 @@ def docker_container(
     docker_client: docker.DockerClient,
     docker_image_key: str,
     container_variables: Dict,
-) -> docker.models.containers.Container:
+) -> Iterator[docker.models.containers.Container]:
     # copy files to input folder, copytree needs to not have the input folder around.
     host_folders["input"].rmdir()
     shutil.copytree(validation_folders["input"], host_folders["input"])
@@ -74,6 +75,7 @@ def docker_container(
         assert host_output_folder.exists()
 
     # run the container (this may take some time)
+    container = None
     try:
         volumes = {
             host_folders[folder]: {
@@ -89,6 +91,7 @@ def docker_container(
             volumes=volumes,
             environment=container_variables,
         )
+        assert isinstance(container, docker.models.containers.Container)
         response = container.wait()
         if response["StatusCode"] > 0:
             logs = container.logs(timestamps=True)
@@ -106,6 +109,7 @@ def docker_container(
             yield container
     except docker.errors.ContainerError as exc:
         # the container did not run correctly
+        assert isinstance(container, docker.models.containers.Container)
         pytest.fail(
             "The container stopped with exit code {}\n\n\ncommand:\n {}, \n\n\nlog:\n{}".format(
                 exc.exit_status,
@@ -118,7 +122,9 @@ def docker_container(
         )
     finally:
         # cleanup
-        container.remove()
+        if container:
+            assert isinstance(container, docker.models.containers.Container)
+            container.remove()
 
 
 def _convert_to_simcore_labels(image_labels: Dict) -> Dict:
@@ -136,30 +142,31 @@ def _convert_to_simcore_labels(image_labels: Dict) -> Dict:
 
 
 def test_run_container(
-    validation_folders: Dict,
-    host_folders: Dict,
+    validation_folders: Dict[str, Path],
+    host_folders: Dict[str, Path],
     docker_container: docker.models.containers.Container,
 ):
     for folder in _FOLDER_NAMES:
-        if folder != "input":
-            # test if the files that should be there are actually there and correct
-            list_of_files = [
-                x.name
-                for x in validation_folders[folder].iterdir()
-                if not ".gitkeep" in x.name
-            ]
-            for file_name in list_of_files:
-                assert Path(
-                    host_folders[folder] / file_name
-                ).exists(), f"missing {file_name=} in {host_folders[folder]=}"
-            match, mismatch, errors = filecmp.cmpfiles(
-                host_folders[folder],
-                validation_folders[folder],
-                list_of_files,
-                shallow=False,
-            )
-            # assert not mismatch, "wrong/incorrect files in {}".format(host_folders[folder])
-            assert not errors, "missing files in {}".format(host_folders[folder])
+        # test if the files that should be there are actually there and correct
+        list_of_files = [
+            x.relative_to(validation_folders[folder])
+            for x in validation_folders[folder].rglob("*")
+            if x.is_file() and not ".gitkeep" in x.name
+        ]
+        for file_name in list_of_files:
+            assert Path(
+                host_folders[folder] / file_name
+            ).exists(), f"missing {file_name=} in {host_folders[folder]=}"
+        match, mismatch, errors = filecmp.cmpfiles(
+            host_folders[folder],
+            validation_folders[folder],
+            list_of_files,
+            shallow=False,
+        )
+        assert match
+        assert not mismatch
+        # assert not mismatch, "wrong/incorrect files in {}".format(host_folders[folder])
+        assert not errors, "missing files in {}".format(host_folders[folder])
 
     # check the output is correct based on container labels
     output_cfg = {}
