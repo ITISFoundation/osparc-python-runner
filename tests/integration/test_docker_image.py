c0f9a8c5e23b62ad10c83cbf0ad61a9b1c2f7272
Andrei Neagu <5694077+GitHK@users.noreply.github.com>
2025-05-21 14:00:20 +0200

    Upgrade metadata (#144)
    upgrading metadata and bumping version

--- tests/integration/test_docker_image.py
+++ tests/integration/test_docker_image.py
@@ -3,6 +3,7 @@
 # pylint:disable=redefined-outer-name
 
 import json
+from copy import deepcopy
 import shutil
 import urllib.request
 from pathlib import Path
@@ -18,7 +19,7 @@ import yaml
 # HELPERS
 def _download_url(url: str, file: Path):
     # Download the file from `url` and save it locally under `file_name`:
-    with urllib.request.urlopen(url) as response, file.open('wb') as out_file:
+    with urllib.request.urlopen(url) as response, file.open("wb") as out_file:
         shutil.copyfileobj(response, out_file)
     assert file.exists()
 
@@ -36,6 +37,7 @@ def _convert_to_simcore_labels(image_labels: Dict) -> Dict:
     assert len(io_simcore_labels) > 0
     return io_simcore_labels
 
+
 # FIXTURES
 @pytest.fixture
 def osparc_service_labels_jsonschema(tmp_path) -> Dict:
@@ -44,19 +46,25 @@ def osparc_service_labels_jsonschema(tmp_path) -> Dict:
     _download_url(url, file_name)
     with file_name.open() as fp:
         json_schema = json.load(fp)
+        # artifically add to legacy scheme new icon field which is optional
+        # NOTE: a prpoer fix is to migrate this repository to using ooil
+        json_schema["properties"]["icon"] = deepcopy(json_schema["properties"]["thumbnail"])
         return json_schema
 
 
-@pytest.fixture(scope='session')
+@pytest.fixture(scope="session")
 def metadata_labels(metadata_file: Path) -> Dict:
     with metadata_file.open() as fp:
         metadata = yaml.safe_load(fp)
         return metadata
 
+
 # TESTS
 
 
-def test_docker_io_simcore_labels_against_files(docker_image: docker.models.images.Image, metadata_labels: Dict):
+def test_docker_io_simcore_labels_against_files(
+    docker_image: docker.models.images.Image, metadata_labels: Dict
+):
     image_labels = docker_image.labels
     io_simcore_labels = _convert_to_simcore_labels(image_labels)
     # check files are identical
@@ -65,16 +73,18 @@ def test_docker_io_simcore_labels_against_files(docker_image: docker.models.imag
         assert value == metadata_labels[key]
 
 
-def test_validate_docker_io_simcore_labels(docker_image: docker.models.images.Image, osparc_service_labels_jsonschema: Dict):
+def test_validate_docker_io_simcore_labels(
+    docker_image: docker.models.images.Image, osparc_service_labels_jsonschema: Dict
+):
     image_labels = docker_image.labels
     # get io labels
     io_simcore_labels = _convert_to_simcore_labels(image_labels)
     # validate schema
     try:
-        jsonschema.validate(io_simcore_labels,
-                            osparc_service_labels_jsonschema)
+        jsonschema.validate(io_simcore_labels, osparc_service_labels_jsonschema)
     except jsonschema.SchemaError:
-        pytest.fail("Schema {} contains errors".format(
-            osparc_service_labels_jsonschema))
+        pytest.fail(
+            "Schema {} contains errors".format(osparc_service_labels_jsonschema)
+        )
     except jsonschema.ValidationError:
         pytest.fail("Failed to validate docker image io labels against schema")
